3daa257a137beec6b1a466e66642e18b93658da7
Pedro Abreu <pedro.aurelio.abreu@gmail.com>
2023-04-26 17:41:57 -0300

    Create Review views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants do
+  resources :restaurants, only: [:new, :create, :index, :show] do
     resources :reviews, only: [:new, :create]
   end
 end
